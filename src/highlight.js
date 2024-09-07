e571d894f9c55174298f66c5b6e881e9d020097c
Matt Mundell <matt@mundell.me>
2024-09-07 23:44:37 +0200

    add LineComment

--- src/highlight.js
+++ src/highlight.js
@@ -2,8 +2,9 @@ import { styleTags, tags as t } from '@lezer/highlight'
 
 export const highlighting = styleTags({
   'Top/Name Special': t.keyword,
-  'Name': t.tagName,
-  'Error': t.special(t.invalid),
+  Name: t.tagName,
+  Error: t.special(t.invalid),
+  LineComment: t.lineComment,
   '( )': t.paren,
   ',': t.separator
 })
