501c8da65efa5477e5c6f39d614742408dcaecdd
Matt Mundell <matt@mundell.me>
2024-09-07 23:35:52 +0200

    hi Error

--- src/highlight.js
+++ src/highlight.js
@@ -3,6 +3,7 @@ import { styleTags, tags as t } from '@lezer/highlight'
 export const highlighting = styleTags({
   'Top/Name Special': t.keyword,
   'Name': t.tagName,
+  'Error': t.special(t.invalid),
   '( )': t.paren,
   ',': t.separator
 })
