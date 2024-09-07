f185d62bf6021a203c12a4625aa1bf59c541dee5
Matt Mundell <matt@mundell.me>
2024-09-08 00:09:09 +0200

    add pretty

--- src/index.js
+++ src/index.js
@@ -24,3 +24,32 @@ function language
 () {
   return new LanguageSupport(lr)
 }
+
+export
+function pretty
+(node, offset = 0, indent = 0) {
+  if (node) {
+    let ret, child, prefix
+
+    ret = ''
+    prefix = ''
+    if (indent)
+      prefix = ' '.repeat(offset)
+    offset += (node.name.length + 1)
+    child = node.firstChild
+    while (child) {
+      let str
+
+      str = pretty(child, offset, ret.length)
+      if (str) {
+        if (ret.length)
+          ret += ',\n'
+        ret += str
+      }
+      child = child.nextSibling
+    }
+
+    return prefix + node.name + (ret.length ? '(' + ret + ')' : '')
+  }
+  return ''
+}
