eb7e30a4011de282aff14c6762ecc62ea07e3989
Matt Mundell <matt@mundell.me>
2024-09-08 20:46:44 +0200

    test pretty

--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
-import {lr} from "../dist/index.js"
-import {fileTests} from "@lezer/generator/dist/test"
+import { lr, pretty } from "../dist/index.js"
+import { fileTests } from "@lezer/generator/dist/test"
 
 import * as fs from "fs"
 import * as path from "path"
 import { fileURLToPath } from 'url'
+import assert from 'node:assert/strict'
+
 let caseDir = path.dirname(fileURLToPath(import.meta.url))
 
 function test1(file) {
@@ -17,11 +19,46 @@ function test1(file) {
   })
 }
 
+function testPretty
+() {
+  let prettyDir
+
+  function isInput
+  (file) {
+    if (file.endsWith('.leztree')) {
+      if (file.endsWith('.leztree.leztree'))
+        return 0
+      return 1
+    }
+    return 0
+  }
+
+  prettyDir = path.join(caseDir, "pretty")
+  describe('pretty', () => {
+    for (let file of fs.readdirSync(prettyDir)) {
+      let content, name
+
+      content = fs.readFileSync(path.join(prettyDir, file), "utf8")
+      if (isInput(file)) {
+        name = /^[^\.]*/.exec(file)[0]
+        it(name, () => {
+          let tree, expected
+
+          expected = fs.readFileSync(path.join(prettyDir, name + '.leztree.leztree'), "utf8")?.trim()
+          tree = lr.parser.parse(content)
+          assert.equal(pretty(tree.topNode), expected)
+        })
+      }
+    }
+  })
+}
+
 function testAll() {
   for (let file of fs.readdirSync(caseDir)) {
     if (!/\.txt$/.test(file)) continue
     test1(file)
   }
+  testPretty()
 }
 
 //console.log(process.argv)
