7291e75120a29bce6e79a69cfcc268caa604a6c2
Matt Mundell <matt@mundell.me>
2024-12-02 16:49:44 +0200

    add back external

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import { lezer } from '@lezer/generator/rollup'
 
 export default {
   input: 'src/index.js',
+  external: /^(?!\.?\/)/, // internal if starts with ./ or /
   output: [ { file: 'dist/index.cjs', format: 'cjs' },
             { dir: './dist', format: 'es' } ],
   plugins: [ lezer(), nodeResolve() ]
